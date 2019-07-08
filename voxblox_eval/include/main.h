d656e2b305f9101778369eb185e2957b08723098
Vinzz98 <vschorp@student.ethz.ch>
2019-07-08 22:54:17 +0200

    voxblox_eval package working

---
+++ voxblox_eval/include/main.h
@@ -0,0 +1,38 @@
+#include <ros/ros.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+namespace voxblox_eval {
+
+class Evaluator {
+ public:
+  Evaluator(ros::NodeHandle& nh);
+  void referenceMapCallback(const pcl::PointCloud<pcl::PointXYZRGB>& msg);
+  void staticMapCallback(const pcl::PointCloud<pcl::PointXYZRGB>& msg);
+  void dynamicMapCallback(const pcl::PointCloud<pcl::PointXYZRGB>& msg);
+
+ private:
+  ros::NodeHandle nh_;
+  ros::Subscriber reference_map_sub_;
+  ros::Subscriber static_map_sub_;
+  ros::Subscriber dynamic_map_sub_;
+
+  pcl::PointCloud<pcl::PointXYZRGB> reference_map_;
+  pcl::PointCloud<pcl::PointXYZRGB> static_map_;
+  pcl::PointCloud<pcl::PointXYZRGB> dynamic_map_;
+  pcl::PointCloud<pcl::PointXYZRGB> seen_points_map_;
+
+  unsigned long int total_static_points_;
+  unsigned long int found_static_points_;
+  unsigned long int not_found_static_points_;
+  float found_static_share_;
+  float not_found_static_share_;
+
+  unsigned long int total_dynamic_points_;
+  unsigned long int found_dynamic_points_;
+  unsigned long int not_found_dynamic_points_;
+  float found_dynamic_share_;
+  float not_found_dynamic_share_;
+};
+
+}
---
+++ voxblox_eval/src/main.cpp
@@ -0,0 +1,95 @@
+#include "main.h"
+
+#include <ros/time.h>
+#include <pcl_ros/point_cloud.h>
+
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "adero_ifm3d_processing");
+  ros::NodeHandle nh("~");
+  voxblox_eval::Evaluator Evaluator(nh);
+  ROS_INFO("main here");
+  ros::spin();
+  return 0;
+}
+
+namespace voxblox_eval {
+  Evaluator::Evaluator(ros::NodeHandle& nh) {
+    nh_ = nh;
+    reference_map_sub_ = nh_.subscribe("/ref_map", 1, &Evaluator::referenceMapCallback, this);
+    static_map_sub_ = nh_.subscribe("/voxblox_node/static_map_pointcloud", 1, &Evaluator::staticMapCallback, this);
+    dynamic_map_sub_ = nh_.subscribe("/voxblox_node/dynamic_map_pointcloud", 1, &Evaluator::dynamicMapCallback, this);
+
+    total_static_points_ = 0;
+    found_static_points_ = 0;
+    not_found_static_points_ = 0;
+    found_static_share_ = 0;
+    not_found_static_share_ = 0;
+    total_dynamic_points_ = 0;
+    found_dynamic_points_ = 0;
+    not_found_dynamic_points_ = 0;
+    found_dynamic_share_ = 0;
+    not_found_dynamic_share_ = 0;
+
+    ROS_INFO("total static points = %i", total_static_points_);
+  }
+
+  void Evaluator::referenceMapCallback(const pcl::PointCloud<pcl::PointXYZRGB>& msg) {
+    reference_map_ = msg;
+    ROS_INFO("Got reference map");
+  }
+
+  void Evaluator::staticMapCallback(const pcl::PointCloud<pcl::PointXYZRGB>& msg) {
+    int total_static_points = 0;
+    for (pcl::PointXYZRGB point : msg) {
+      total_static_points++;
+      bool found = false;
+      bool already_seen = false;
+      for (pcl::PointXYZRGB reference_point : reference_map_) {
+        if (point.x == reference_point.x && point.y == reference_point.y) {
+          for (pcl::PointXYZRGB seen_point : seen_points_map_) {
+            if (seen_point.x == reference_point.x && seen_point.y == reference_point.y) {
+              already_seen = true;
+            }
+          }
+          if (!already_seen) seen_points_map_.push_back(reference_point);
+          found = true;
+          //ROS_INFO("found");
+          break;
+        }
+      }
+      if (found) found_static_points_++;
+      else not_found_static_points_++;
+    }
+    total_static_points_ += total_static_points;
+
+    found_static_share_ = (float) found_static_points_ / (float) total_static_points_;
+    not_found_static_share_ = (float) not_found_static_points_ / (float) total_static_points_;
+    ROS_INFO("found static share: %f", found_static_share_);
+    ROS_INFO("not found static share: %f", not_found_static_share_);
+    ROS_INFO("reference map size = %i", reference_map_.size());
+    ROS_INFO("number of seen points = %i", seen_points_map_.size());
+  }
+
+  void Evaluator::dynamicMapCallback(const pcl::PointCloud<pcl::PointXYZRGB>& msg) {
+    int total_dynamic_points = 0;
+    for (pcl::PointXYZRGB point : msg) {
+      total_dynamic_points++;
+      bool found = false;
+      for (pcl::PointXYZRGB reference_point : reference_map_) {
+        if (point.x == reference_point.x && point.y == reference_point.y) {
+          found = true;
+          break;
+        }
+      }
+      if (found) found_dynamic_points_++;
+      else not_found_dynamic_points_++;
+    }
+    total_dynamic_points_ += total_dynamic_points;
+    found_dynamic_share_ = (float) found_dynamic_points_ / (float) total_dynamic_points_;
+    not_found_dynamic_share_ = (float) not_found_dynamic_points_ / (float) total_dynamic_points_;
+    ROS_INFO("dynamic points being static share: %f", found_dynamic_share_);
+    ROS_INFO("dynamic points not in static share: %f", not_found_dynamic_share_);
+  }
+
+}
